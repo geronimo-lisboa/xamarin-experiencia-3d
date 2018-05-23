baefb813376ed620a12fd5b1b4e1c7c2b4a67b0f
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-05-22 22:51:01 -0300

    Initial check-in of module TesteApp3DVisualStudio

---
+++ TesteApp3DVisualStudio/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace TesteApp3DVisualStudio
+{
+    // the ConfigurationChanges flags set here keep the EGL context
+    // from being destroyed whenever the device is rotated or the
+    // keyboard is shown (highly recommended for all GL apps)
+    [Activity(Label = "TesteApp3DVisualStudio",
+                ConfigurationChanges = ConfigChanges.KeyboardHidden,
+                ScreenOrientation = ScreenOrientation.SensorLandscape,
+                MainLauncher = true,
+                Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        GLView1 view;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var manager = GetSystemService(Context.ActivityService) as ActivityManager;
+            if (manager.DeviceConfigurationInfo.ReqGlEsVersion >= 0x30000)
+            {
+                // Create our OpenGL view, and display it
+                view = new GLView1(this);
+                SetContentView(view);
+            }
+            else
+                SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
+
